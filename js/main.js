b0f2b9feaa3dedac25da88a0b691d46a5f1f08b0
M-Hatem98 <moh.hat226@gmail.com>
2025-05-10 20:29:41 +0300

    some anime

--- js/main.js
+++ js/main.js
@@ -9,3 +9,48 @@ navLinks[i].addEventListener('click' , function(e){
         
     })
     }
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+let home = document.querySelector('.home')
+if(home) {
+
+    gsap.from(".testimonial", {
+        scrollTrigger: {
+    trigger: ".testimonial",
+    start: "top 80%", // when top of element hits 80% of viewport
+    toggleActions: "play none none none"
+  },
+  opacity: 0,
+//   y: 100,            // start lower and slide up
+  duration: 1.2,
+  stagger: 0.3,
+  ease: "power2.out"
+});
+
+gsap.from(".value-box", {
+    scrollTrigger: {
+        trigger: ".value-box",
+        start: "top 80%", // when top of element hits 80% of viewport
+        toggleActions: "play none none none"
+    },
+    opacity: 0,
+    y: 100,            // start lower and slide up
+    duration: 1.2,
+    stagger: 0.3,
+    ease: "power2.out"
+});
+}
+gsap.from(".card", {
+    scrollTrigger: {
+        trigger: ".card",
+        start: "top 80%", // when top of element hits 80% of viewport
+        toggleActions: "play none none none"
+    },
+    opacity: 0,
+    y: 100,            // start lower and slide up
+    duration: 1.2,
+    stagger: 0.3,
+    ease: "power2.out"
+    });
